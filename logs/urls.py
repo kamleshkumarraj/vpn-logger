a39a4bbad69ca0be0870cf36709776956795f264
Kamlesh Kumar <kamlesh.22jics061@jietjodhpur.ac.in>
2026-01-08 23:10:59 +0530

    we resolve error

--- logs/urls.py
+++ logs/urls.py
@@ -2,12 +2,13 @@ from django.urls import path
 from .views import (
     VPNSessionCreateView,
     VPNSessionUpdateView,
-    VPNSessionDeleteView
+    VPNSessionDeleteView,
+    vpn_dashboard
 )
 
 urlpatterns = [
     path("vpn-sessions/", VPNSessionCreateView.as_view()),
     path("vpn-sessions/<int:pk>/", VPNSessionUpdateView.as_view()),
     path("vpn-sessions/<int:pk>/delete/", VPNSessionDeleteView.as_view()),
-    path("vpn-sessions/show/", )
+    path("vpn-sessions/show/", vpn_dashboard)
 ]
